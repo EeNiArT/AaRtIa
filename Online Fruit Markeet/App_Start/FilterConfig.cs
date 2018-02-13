a33213a4f4ca69634e5187eba970f570fa5a8069
MSNadeem <36435128+MSNadeem@users.noreply.github.com>
2018-02-13 15:57:58 +0500

    Add project files.

---
+++ Online Fruit Markeet/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Fruit_Markeet
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Online Fruit Markeet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Online_Fruit_Markeet.MvcApplication" Language="C#" %>
